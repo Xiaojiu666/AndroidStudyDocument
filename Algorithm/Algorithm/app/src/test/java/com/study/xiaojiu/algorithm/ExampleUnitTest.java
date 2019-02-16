67ee32a3ff9fcac610135a1667bbfc666f16ea63
Xiaojiu666 <417987689@qq.com>
2019-02-16 18:59:48 +0800

    Signed-off-by: Xiaojiu666 <417987689@qq.com>

--- Algorithm/Algorithm/app/src/test/java/com/study/xiaojiu/algorithm/ExampleUnitTest.java
+++ Algorithm/Algorithm/app/src/test/java/com/study/xiaojiu/algorithm/ExampleUnitTest.java
@@ -2,6 +2,8 @@ package com.study.xiaojiu.algorithm;
 
 import org.junit.Test;
 
+import java.util.HashSet;
+
 import static org.junit.Assert.*;
 
 /**
@@ -15,7 +17,17 @@ public class ExampleUnitTest {
         assertEquals(4, 2 + 2);
         //   System.out.println(SimpleAlgorithm.isHappyNumber(4));
         //System.out.println(SimpleAlgorithm.flipWord("AA bb cc dd"));
-       // System.out.println(SimpleAlgorithm.aplusb(6, 1));
+        // System.out.println(SimpleAlgorithm.aplusb(6, 1));
         System.out.println(SimpleAlgorithm.digitCounts(9, 999));
+    }
+    @Test
+    public void test_hasSet() {
+        HashSet<String> objects = new HashSet<>();
+        objects.add("AA");
+        objects.add("AA");
+        for (String object :
+                objects) {
+            System.out.println(object);
+        }
     }
 }
